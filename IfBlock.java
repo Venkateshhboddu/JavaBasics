6a9d6373dc0c656c8cb046743aa8876fe76db87f
venkateshv29 <venkateshv29@gmail.com>
2025-01-23 18:38:16 +0530

    If and If else Block

---
+++ IfBlock.java
@@ -0,0 +1,16 @@
+package basicprograms.JavaBasics;
+
+public class IfBlock {
+
+	public static void main(String[] args) {
+		int rajeshMarks =95;
+		
+		int vkMarks =80;
+		
+		if(rajeshMarks > vkMarks) {
+			
+			System.out.println("Rajesh Got More Marks Than vk");
+		}
+	}
+
+}
